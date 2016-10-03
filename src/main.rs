929fc966a2512693a3eef18ccf251b4e7bcbe713
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-10-03 16:53:58 +0900

    Add example

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ use ndarray::prelude::*;
 use linalg::SquareMatrix;
 
 fn main() {
-    let a2 = arr2(&[[3.0, 1.0, 1.0], [1.0, 3.0, 1.0], [1.0, 1.0, 3.0]]);
-    let (e, vecs) = a2.eig();
+    let a = arr2(&[[3.0, 1.0, 1.0], [1.0, 3.0, 1.0], [1.0, 1.0, 3.0]]);
+    let (e, vecs) = a.eig();
     println!("eigenvalues = {:?}", e);
     println!("eigenvectors = \n{:?}", vecs);
 }

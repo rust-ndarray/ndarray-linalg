f8ebfa9152b72e800a9074c4a7f5265228c9cba0
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-10-03 15:16:34 +0900

    Split into lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,34 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {}
+
+extern crate ndarray;
+extern crate lapack;
+
+use ndarray::prelude::*;
+use lapack::fortran::*;
+
+fn eigs_(n: usize, mut a: Vec<f64>) -> (Vec<f64>, Vec<f64>) {
+    let mut w = vec![0.0; n as usize];
+    let mut work = vec![0.0; 4 * n as usize];
+    let lwork = 4 * n;
+    let mut info = 0;
+    dsyev(b'V',
+          b'U',
+          n as i32,
+          &mut a,
+          n as i32,
+          &mut w,
+          &mut work,
+          lwork as i32,
+          &mut info);
+    assert_eq!(info, 0);
+    (w, a)
+}
+
+pub fn eigs(a: Array<f64, (Ix, Ix)>) -> (Array<f64, Ix>, Array<f64, (Ix, Ix)>) {
+    let rows = a.rows();
+    let cols = a.cols();
+    assert_eq!(rows, cols);
+    let (e, vecs) = eigs_(rows, a.into_raw_vec());
+    let ea = Array::from_vec(e);
+    let va = Array::from_vec(vecs).into_shape((rows, cols)).unwrap();
+    (ea, va)
 }

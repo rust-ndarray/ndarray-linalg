9a0264549773249efdca0b271bf9d6cd3fb38994
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-10-04 03:16:42 +0900

    Add Eigh traits for implement generic eigh

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 
+extern crate num_traits;
 extern crate ndarray;
 extern crate lapack;
 
 use ndarray::prelude::*;
 use lapack::fortran::*;
+use num_traits::Zero;
 
 pub trait Matrix: Sized {
     type Vector;
@@ -17,36 +19,73 @@ pub trait SquareMatrix: Matrix {
     fn eigh(self) -> (Self::Vector, Self);
 }
 
-impl Matrix for Array<f64, (Ix, Ix)> {
-    type Vector = Array<f64, Ix>;
+impl<A> Matrix for Array<A, (Ix, Ix)> {
+    type Vector = Array<A, Ix>;
 }
 
-fn eigh_(n: usize, mut a: Vec<f64>) -> (Vec<f64>, Vec<f64>) {
-    let mut w = vec![0.0; n as usize];
-    let mut work = vec![0.0; 4 * n as usize];
-    let lwork = 4 * n;
-    let mut info = 0;
-    dsyev(b'V',
-          b'U',
-          n as i32,
-          &mut a,
-          n as i32,
-          &mut w,
-          &mut work,
-          lwork as i32,
-          &mut info);
-    assert_eq!(info, 0);
-    (w, a)
+pub trait Eigh: Sized {
+    fn syev(jobz: u8,
+            uplo: u8,
+            n: i32,
+            a: &mut [Self],
+            lda: i32,
+            w: &mut [Self],
+            work: &mut [Self],
+            lwork: i32,
+            info: &mut i32);
 }
 
-impl SquareMatrix for Array<f64, (Ix, Ix)> {
+impl Eigh for f64 {
+    fn syev(jobz: u8,
+            uplo: u8,
+            n: i32,
+            a: &mut [f64],
+            lda: i32,
+            w: &mut [f64],
+            work: &mut [f64],
+            lwork: i32,
+            info: &mut i32) {
+        dsyev(jobz, uplo, n, a, lda, w, work, lwork, info);
+    }
+}
+
+impl Eigh for f32 {
+    fn syev(jobz: u8,
+            uplo: u8,
+            n: i32,
+            a: &mut [f32],
+            lda: i32,
+            w: &mut [f32],
+            work: &mut [f32],
+            lwork: i32,
+            info: &mut i32) {
+        ssyev(jobz, uplo, n, a, lda, w, work, lwork, info);
+    }
+}
+
+impl<A: Eigh + Clone + Zero> SquareMatrix for Array<A, (Ix, Ix)> {
     fn eigh(self) -> (Self::Vector, Self) {
         let rows = self.rows();
         let cols = self.cols();
         assert_eq!(rows, cols);
-        let (e, vecs) = eigh_(rows, self.into_raw_vec());
-        let ea = Array::from_vec(e);
-        let va = Array::from_vec(vecs).into_shape((rows, cols)).unwrap();
+
+        let mut a = self.into_raw_vec();
+        let n = rows as i32;
+        let mut w: Vec<A> = vec![A::zero(); n as usize];
+        let mut work: Vec<A> = vec![A::zero(); 4 * n as usize];
+        let mut info = 0;
+        Eigh::syev(b'V',
+                   b'U',
+                   n,
+                   &mut a,
+                   n,
+                   &mut w,
+                   &mut work,
+                   4 * n,
+                   &mut info);
+
+        let ea = Array::from_vec(w);
+        let va = Array::from_vec(a).into_shape((rows, cols)).unwrap();
         (ea, va)
     }
 }

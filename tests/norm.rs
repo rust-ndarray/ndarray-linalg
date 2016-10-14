dd21cb77ecf32664e23d5aea0da1e850ad911d82
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-10-14 11:11:11 +0900

    Add test for Frobenius norm

--- tests/norm.rs
+++ tests/norm.rs
@@ -1,9 +1,11 @@
 
 extern crate ndarray;
 extern crate ndarray_linalg as linalg;
+extern crate num_traits;
 
 use ndarray::prelude::*;
 use linalg::Matrix;
+use num_traits::float::Float;
 
 fn assert_almost_eq(a: f64, b: f64) {
     let rel_dev = (a - b).abs() / (a.abs() + b.abs());
@@ -17,6 +19,7 @@ fn test_matrix_norm_square() {
     let a = Array::range(1., 10., 1.).into_shape((3, 3)).unwrap();
     assert_almost_eq(a.norm_1(), 18.0);
     assert_almost_eq(a.norm_i(), 24.0);
+    assert_almost_eq(a.norm_f(), 285.0.sqrt());
 }
 
 #[test]
@@ -24,34 +27,43 @@ fn test_matrix_norm_square_t() {
     let a = Array::range(1., 10., 1.).into_shape((3, 3)).unwrap().reversed_axes();
     assert_almost_eq(a.norm_1(), 24.0);
     assert_almost_eq(a.norm_i(), 18.0);
+    assert_almost_eq(a.norm_f(), 285.0.sqrt());
 }
 
 #[test]
 fn test_matrix_norm_3x4() {
-    let a = arr2(&[[3.0, 1.0, 1.0, 1.0], [1.0, 3.0, 1.0, 1.0], [1.0, 1.0, 3.0, 1.0]]);
-    assert_almost_eq(a.norm_1(), 5.0);
-    assert_almost_eq(a.norm_i(), 6.0);
+    let a = Array::range(1., 13., 1.).into_shape((3, 4)).unwrap();
+    assert_almost_eq(a.norm_1(), 24.0);
+    assert_almost_eq(a.norm_i(), 42.0);
+    assert_almost_eq(a.norm_f(), 650.0.sqrt());
 }
 
 #[test]
 fn test_matrix_norm_3x4_t() {
-    let a = arr2(&[[3.0, 1.0, 1.0, 1.0], [1.0, 3.0, 1.0, 1.0], [1.0, 1.0, 3.0, 1.0]])
+    let a = Array::range(1., 13., 1.)
+        .into_shape((3, 4))
+        .unwrap()
         .reversed_axes();
-    assert_almost_eq(a.norm_1(), 6.0);
-    assert_almost_eq(a.norm_i(), 5.0);
+    assert_almost_eq(a.norm_1(), 42.0);
+    assert_almost_eq(a.norm_i(), 24.0);
+    assert_almost_eq(a.norm_f(), 650.0.sqrt());
 }
 
 #[test]
 fn test_matrix_norm_4x3() {
-    let a = arr2(&[[3.0, 1.0, 1.0], [1.0, 3.0, 1.0], [1.0, 1.0, 3.0], [1.0, 1.0, 1.0]]);
-    assert_almost_eq(a.norm_1(), 6.0);
-    assert_almost_eq(a.norm_i(), 5.0);
+    let a = Array::range(1., 13., 1.).into_shape((4, 3)).unwrap();
+    assert_almost_eq(a.norm_1(), 30.0);
+    assert_almost_eq(a.norm_i(), 33.0);
+    assert_almost_eq(a.norm_f(), 650.0.sqrt());
 }
 
 #[test]
 fn test_matrix_norm_4x3_t() {
-    let a = arr2(&[[3.0, 1.0, 1.0], [1.0, 3.0, 1.0], [1.0, 1.0, 3.0], [1.0, 1.0, 1.0]])
+    let a = Array::range(1., 13., 1.)
+        .into_shape((4, 3))
+        .unwrap()
         .reversed_axes();
-    assert_almost_eq(a.norm_1(), 5.0);
-    assert_almost_eq(a.norm_i(), 6.0);
+    assert_almost_eq(a.norm_1(), 33.0);
+    assert_almost_eq(a.norm_i(), 30.0);
+    assert_almost_eq(a.norm_f(), 650.0.sqrt());
 }

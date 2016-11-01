f1519d57621837f9caca81ea2c8033bd9a27d213
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-11-01 16:41:18 +0900

    Add test for eigh

--- tests/eigh.rs
+++ tests/eigh.rs
@@ -5,8 +5,16 @@ extern crate ndarray_linalg as linalg;
 use ndarray::prelude::*;
 use linalg::SquareMatrix;
 
+fn assert_almost_eq(a: f64, b: f64) {
+    let rel_dev = (a - b).abs() / (a.abs() + b.abs());
+    if rel_dev > 1.0e-7 {
+        panic!("a={:?}, b={:?} are not almost equal", a, b);
+    }
+}
+
+
 #[test]
-fn test_eigh() {
+fn eigen_vector_manual() {
     let a = arr2(&[[3.0, 1.0, 1.0], [1.0, 3.0, 1.0], [1.0, 1.0, 3.0]]);
     let (e, vecs) = a.clone().eigh().unwrap();
     assert!(e.all_close(&arr1(&[2.0, 2.0, 5.0]), 1.0e-7));
@@ -14,5 +22,15 @@ fn test_eigh() {
         let av = a.dot(&v);
         let ev = v.mapv(|x| e[i] * x);
         assert!(av.all_close(&ev, 1.0e-7));
+    }
+}
+
+#[test]
+fn diagonalize() {
+    let a = arr2(&[[3.0, 1.0, 1.0], [1.0, 3.0, 1.0], [1.0, 1.0, 3.0]]);
+    let (e, vecs) = a.clone().eigh().unwrap();
+    let s = vecs.t().dot(&a).dot(&vecs);
+    for i in 0..3 {
+        assert_almost_eq(e[i], s[(i, i)]);
     }
 }

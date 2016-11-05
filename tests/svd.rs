1034dcb2c2efe82785ac728a1e4696a9a912a6e5
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-11-05 18:38:06 +0900

    Add test for 4x3 / 3x4 matrices (not passed)

--- tests/svd.rs
+++ tests/svd.rs
@@ -39,3 +39,49 @@ fn svd_square_t() {
     }
     all_close(u.dot(&sm).dot(&vt), a);
 }
+
+#[test]
+fn svd_4x3() {
+    let r_dist = Range::new(0., 1.);
+    let a = Array::<f64, _>::random((4, 3), r_dist);
+    let (u, s, vt) = a.clone().svd().unwrap();
+    let mut sm = Array::eye(3);
+    for i in 0..3 {
+        sm[(i, i)] = s[i];
+    }
+    all_close(u.dot(&sm).dot(&vt), a);
+}
+#[test]
+fn svd_4x3_t() {
+    let r_dist = Range::new(0., 1.);
+    let a = Array::<f64, _>::random((4, 3), r_dist).reversed_axes();
+    let (u, s, vt) = a.clone().svd().unwrap();
+    let mut sm = Array::eye(3);
+    for i in 0..3 {
+        sm[(i, i)] = s[i];
+    }
+    all_close(u.dot(&sm).dot(&vt), a);
+}
+
+#[test]
+fn svd_3x4() {
+    let r_dist = Range::new(0., 1.);
+    let a = Array::<f64, _>::random((3, 4), r_dist);
+    let (u, s, vt) = a.clone().svd().unwrap();
+    let mut sm = Array::eye(3);
+    for i in 0..3 {
+        sm[(i, i)] = s[i];
+    }
+    all_close(u.dot(&sm).dot(&vt), a);
+}
+#[test]
+fn svd_3x4_t() {
+    let r_dist = Range::new(0., 1.);
+    let a = Array::<f64, _>::random((3, 4), r_dist).reversed_axes();
+    let (u, s, vt) = a.clone().svd().unwrap();
+    let mut sm = Array::eye(3);
+    for i in 0..3 {
+        sm[(i, i)] = s[i];
+    }
+    all_close(u.dot(&sm).dot(&vt), a);
+}

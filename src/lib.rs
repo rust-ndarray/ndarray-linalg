f743aaca18904c14fd0563de21adc475c0772bf4
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-11-11 20:41:31 +0900

    Add crate document

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+//! This crate serves matrix manipulation for [rust-ndarray](https://github.com/bluss/rust-ndarray)
+//! They are implemented as traits,
+//! [Matrix](matrix/trait.Matrix.html), [SquareMatrix](square/trait.SquareMatrix.html), and
+//! [HermiteMatrix](hermite/trait.HermiteMatrix.html)
+//!
+//! Matrix
+//! -------
+//! - [singular-value decomposition](matrix/trait.Matrix.html#tymethod.svd)
+//! - [QR decomposition](matrix/trait.Matrix.html#tymethod.qr)
+//! - [operator norm for L1 norm](matrix/trait.Matrix.html#tymethod.norm_1)
+//! - [operator norm for L-inf norm](matrix/trait.Matrix.html#tymethod.norm_i)
+//! - [Frobeiuns norm](matrix/trait.Matrix.html#tymethod.norm_f)
+//!
+//! SquareMatrix
+//! -------------
+//! - [inverse of matrix](square/trait.SquareMatrix.html#tymethod.inv)
+//! - [trace of matrix](square/trait.SquareMatrix.html#tymethod.trace)
+//! - [WIP] eigenvalue
+//! - [WIP] LU factorization
+//!
+//! HermiteMatrix
+//! --------------
+//! - [eigenvalue analysis](hermite/trait.HermiteMatrix.html#tymethod.eigh)
+//! - [symmetric square root](hermite/trait.HermiteMatrix.html#tymethod.ssqrt)
+//! - [WIP] Cholesky factorization
 
 extern crate ndarray;
 extern crate num_traits;

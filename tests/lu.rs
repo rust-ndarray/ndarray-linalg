3c508c4c983ff53c14233f96e57addb09c90803b
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-11-19 18:21:01 +0900

    Compress duplicated codes for testing LU

--- tests/lu.rs
+++ tests/lu.rs
@@ -28,8 +28,7 @@ fn $testname() {
     println!("permutated = \n{:?}", &pa);
     all_close(pa, arr2($answer))
 }
-    }
-}
+}} // end test_permutate
 
 macro_rules! test_permutate_t {
     ($testname:ident, $permutate:expr, $input:expr, $answer:expr) => {
@@ -42,8 +41,7 @@ fn $testname() {
     println!("permutated = \n{:?}", &pa);
     all_close(pa, arr2($answer))
 }
-    }
-}
+}} // end test_permutate_t
 
 test_permutate!(permutate,
                 vec![2, 2, 3],
@@ -70,201 +68,89 @@ test_permutate_t!(permutate_4x3_t,
                   &[[1., 4., 7., 10.], [2., 5., 8., 11.], [3., 6., 9., 12.]],
                   &[[10., 11., 12.], [4., 5., 6.], [7., 8., 9.], [1., 2., 3.]]);
 
-#[test]
-fn lu_square_upper() {
-    let r_dist = Range::new(0., 1.);
-    let mut a = Array::<f64, _>::random((3, 3), r_dist);
-    for ((i, j), val) in a.indexed_iter_mut() {
-        if i > j {
-            *val = 0.0;
-        }
-    }
+fn test_lu(a: Array<f64, (Ix, Ix)>) {
     println!("a = \n{:?}", &a);
     let (p, l, u) = a.clone().lu().unwrap();
     println!("P = \n{:?}", &p);
     println!("L = \n{:?}", &l);
     println!("U = \n{:?}", &u);
+    println!("LU = \n{:?}", l.dot(&u));
     all_close(l.dot(&u).permutated(&p), a);
 }
 
+macro_rules! test_lu_upper {
+    ($testname:ident, $testname_t:ident, $n:expr, $m:expr) => {
 #[test]
-fn lu_square_upper_t() {
+fn $testname() {
     let r_dist = Range::new(0., 1.);
-    let mut a = Array::<f64, _>::random((3, 3), r_dist).reversed_axes();
+    let mut a = Array::<f64, _>::random(($n, $m), r_dist);
     for ((i, j), val) in a.indexed_iter_mut() {
         if i > j {
             *val = 0.0;
         }
     }
-    println!("a = \n{:?}", &a);
-    let (p, l, u) = a.clone().lu().unwrap();
-    println!("P = \n{:?}", &p);
-    println!("L = \n{:?}", &l);
-    println!("U = \n{:?}", &u);
-    all_close(l.dot(&u).permutated(&p), a);
+    test_lu(a);
 }
-
 #[test]
-fn lu_square_lower() {
+fn $testname_t() {
     let r_dist = Range::new(0., 1.);
-    let mut a = Array::<f64, _>::random((3, 3), r_dist);
+    let mut a = Array::<f64, _>::random(($m, $n), r_dist).reversed_axes();
+    for ((i, j), val) in a.indexed_iter_mut() {
+        if i > j {
+            *val = 0.0;
+        }
+    }
+    test_lu(a);
+}
+}} // end test_lu_upper
+test_lu_upper!(lu_square_upper, lu_square_upper_t, 3, 3);
+test_lu_upper!(lu_3x4_upper, lu_3x4_upper_t, 3, 4);
+test_lu_upper!(lu_4x3_upper, lu_4x3_upper_t, 4, 3);
+
+macro_rules! test_lu_lower {
+    ($testname:ident, $testname_t:ident, $n:expr, $m:expr) => {
+#[test]
+fn $testname() {
+    let r_dist = Range::new(0., 1.);
+    let mut a = Array::<f64, _>::random(($n, $m), r_dist);
     for ((i, j), val) in a.indexed_iter_mut() {
         if i < j {
             *val = 0.0;
         }
     }
-    println!("a = \n{:?}", &a);
-    let (p, l, u) = a.clone().lu().unwrap();
-    println!("P = \n{:?}", &p);
-    println!("L = \n{:?}", &l);
-    println!("U = \n{:?}", &u);
-    println!("LU = \n{:?}", l.dot(&u));
-    all_close(l.dot(&u).permutated(&p), a);
+    test_lu(a);
 }
-
 #[test]
-fn lu_square_lower_t() {
+fn $testname_t() {
     let r_dist = Range::new(0., 1.);
-    let mut a = Array::<f64, _>::random((3, 3), r_dist).reversed_axes();
+    let mut a = Array::<f64, _>::random(($m, $n), r_dist).reversed_axes();
     for ((i, j), val) in a.indexed_iter_mut() {
         if i < j {
             *val = 0.0;
         }
     }
-    println!("a = \n{:?}", &a);
-    let (p, l, u) = a.clone().lu().unwrap();
-    println!("P = \n{:?}", &p);
-    println!("L = \n{:?}", &l);
-    println!("U = \n{:?}", &u);
-    println!("LU = \n{:?}", l.dot(&u));
-    all_close(l.dot(&u).permutated(&p), a);
+    test_lu(a);
 }
+}} // end test_lu_lower
+test_lu_lower!(lu_square_lower, lu_square_lower_t, 3, 3);
+test_lu_lower!(lu_3x4_lower, lu_3x4_lower_t, 3, 4);
+test_lu_lower!(lu_4x3_lower, lu_4x3_lower_t, 4, 3);
 
+macro_rules! test_lu {
+    ($testname:ident, $testname_t:ident, $n:expr, $m:expr) => {
 #[test]
-fn lu_square() {
+fn $testname() {
     let r_dist = Range::new(0., 1.);
-    let a = Array::<f64, _>::random((3, 3), r_dist);
-    println!("a = \n{:?}", &a);
-    let (p, l, u) = a.clone().lu().unwrap();
-    println!("P = \n{:?}", &p);
-    println!("L = \n{:?}", &l);
-    println!("U = \n{:?}", &u);
-    println!("LU = \n{:?}", l.dot(&u));
-    all_close(l.dot(&u).permutated(&p), a);
+    let a = Array::<f64, _>::random(($n, $m), r_dist);
+    test_lu(a);
 }
-
 #[test]
-fn lu_square_t() {
+fn $testname_t() {
     let r_dist = Range::new(0., 1.);
-    let a = Array::<f64, _>::random((3, 3), r_dist).reversed_axes();
-    println!("a = \n{:?}", &a);
-    let (p, l, u) = a.clone().lu().unwrap();
-    println!("P = \n{:?}", &p);
-    println!("L = \n{:?}", &l);
-    println!("U = \n{:?}", &u);
-    all_close(l.dot(&u).permutated(&p), a);
+    let a = Array::<f64, _>::random(($m, $n), r_dist).reversed_axes();
+    test_lu(a);
 }
-
-// #[test]
-// fn lu_3x4() {
-//     let r_dist = Range::new(0., 1.);
-//     let a = Array::<f64, _>::random((3, 4), r_dist);
-//     println!("a = \n{:?}", &a);
-//     let (p, l, u) = a.clone().lu().unwrap();
-//     println!("P = \n{:?}", &p);
-//     println!("L = \n{:?}", &l);
-//     println!("U = \n{:?}", &u);
-//     println!("LU = \n{:?}", l.dot(&u));
-//     all_close(l.dot(&u).permutated(&p), a);
-// }
-//
-// #[test]
-// fn lu_3x4_t() {
-//     let r_dist = Range::new(0., 1.);
-//     let a = Array::<f64, _>::random((4, 3), r_dist).reversed_axes();
-//     println!("a = \n{:?}", &a);
-//     let (p, l, u) = a.clone().lu().unwrap();
-//     println!("P = \n{:?}", &p);
-//     println!("L = \n{:?}", &l);
-//     println!("U = \n{:?}", &u);
-//     all_close(l.dot(&u).permutated(&p), a);
-// }
-
-// #[test]
-// fn lu_4x3_upper() {
-//     let r_dist = Range::new(0., 1.);
-//     let mut a = Array::<f64, _>::random((4, 3), r_dist);
-//     for ((i, j), val) in a.indexed_iter_mut() {
-//         if i > j {
-//             *val = 0.0;
-//         }
-//     }
-//     println!("a = \n{:?}", &a);
-//     let (p, l, u) = a.clone().lu().unwrap();
-//     println!("P = \n{:?}", &p);
-//     println!("L = \n{:?}", &l);
-//     println!("U = \n{:?}", &u);
-//     println!("LU = \n{:?}", l.dot(&u));
-//     all_close(l.dot(&u).permutated(&p), a);
-// }
-//
-// #[test]
-// fn lu_4x3_lower() {
-//     let r_dist = Range::new(0., 1.);
-//     let mut a = Array::<f64, _>::random((4, 3), r_dist);
-//     for ((i, j), val) in a.indexed_iter_mut() {
-//         if i < j {
-//             *val = 0.0;
-//         }
-//     }
-//     println!("a = \n{:?}", &a);
-//     let (p, l, u) = a.clone().lu().unwrap();
-//     println!("P = \n{:?}", &p);
-//     println!("L = \n{:?}", &l);
-//     println!("U = \n{:?}", &u);
-//     println!("LU = \n{:?}", l.dot(&u));
-//     all_close(l.dot(&u).permutated(&p), a);
-// }
-
-#[test]
-fn lu_4x3_upper_t() {
-    let r_dist = Range::new(0., 1.);
-    let mut a = Array::<f64, _>::random((3, 4), r_dist).reversed_axes();
-    for ((i, j), val) in a.indexed_iter_mut() {
-        if i > j {
-            *val = 0.0;
-        }
-    }
-    println!("a = \n{:?}", &a);
-    let (p, l, u) = a.clone().lu().unwrap();
-    println!("P = \n{:?}", &p);
-    println!("L = \n{:?}", &l);
-    println!("U = \n{:?}", &u);
-    println!("LU = \n{:?}", l.dot(&u));
-    all_close(l.dot(&u).permutated(&p), a);
-}
-
-// #[test]
-// fn lu_4x3() {
-//     let r_dist = Range::new(0., 1.);
-//     let a = Array::<f64, _>::random((4, 3), r_dist);
-//     println!("a = \n{:?}", &a);
-//     let (p, l, u) = a.clone().lu().unwrap();
-//     println!("P = \n{:?}", &p);
-//     println!("L = \n{:?}", &l);
-//     println!("U = \n{:?}", &u);
-//     println!("LU = \n{:?}", l.dot(&u));
-//     all_close(l.dot(&u).permutated(&p), a);
-// }
-//
-// #[test]
-// fn lu_4x3_t() {
-//     let r_dist = Range::new(0., 1.);
-//     let a = Array::<f64, _>::random((3, 4), r_dist).reversed_axes();
-//     println!("a = \n{:?}", &a);
-//     let (p, l, u) = a.clone().lu().unwrap();
-//     println!("P = \n{:?}", &p);
-//     println!("L = \n{:?}", &l);
-//     println!("U = \n{:?}", &u);
-//     all_close(l.dot(&u).permutated(&p), a);
-// }
+}} // end test_lu
+test_lu!(lu_square, lu_square_t, 3, 3);
+test_lu!(lu_3x4, lu_3x4_t, 3, 4);
+test_lu!(lu_4x3, lu_4x3_t, 4, 3);

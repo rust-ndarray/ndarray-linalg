cd66a3dfacd96698b767b7a581891b541e88564c
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-11-19 18:04:03 +0900

    Use macro for testing permutate

--- tests/lu.rs
+++ tests/lu.rs
@@ -16,73 +16,59 @@ fn all_close(a: Array<f64, (Ix, Ix)>, b: Array<f64, (Ix, Ix)>) {
                b);
     }
 }
+
+macro_rules! test_permutate {
+    ($testname:ident, $permutate:expr, $input:expr, $answer:expr) => {
 #[test]
-fn permutate() {
-    let a = arr2(&[[1., 2., 3.], [4., 5., 6.], [7., 8., 9.]]);
+fn $testname() {
+    let a = arr2($input);
     println!("a= \n{:?}", &a);
-    let p = vec![2, 2, 3]; // replace 1-2
+    let p = $permutate; // replace 1-2
     let pa = a.permutated(&p);
     println!("permutated = \n{:?}", &pa);
-    all_close(pa, arr2(&[[4., 5., 6.], [1., 2., 3.], [7., 8., 9.]]))
+    all_close(pa, arr2($answer))
+}
+    }
 }
 
+macro_rules! test_permutate_t {
+    ($testname:ident, $permutate:expr, $input:expr, $answer:expr) => {
 #[test]
-fn permutate_t() {
-    let a = arr2(&[[1., 4., 7.], [2., 5., 8.], [3., 6., 9.]]).reversed_axes();
+fn $testname() {
+    let a = arr2($input).reversed_axes();
     println!("a= \n{:?}", &a);
-    let p = vec![2, 2, 3]; // replace 1-2
+    let p = $permutate; // replace 1-2
     let pa = a.permutated(&p);
     println!("permutated = \n{:?}", &pa);
-    all_close(pa, arr2(&[[4., 5., 6.], [1., 2., 3.], [7., 8., 9.]]))
+    all_close(pa, arr2($answer))
+}
+    }
 }
 
-#[test]
-fn permutate_3x4() {
-    let a = arr2(&[[1., 4., 7., 10.], [2., 5., 8., 11.], [3., 6., 9., 12.]]);
-    println!("a= \n{:?}", &a);
-    let p = vec![1, 3, 3]; // replace 2-3
-    println!("permutation = \n{:?}", &p);
-    let pa = a.permutated(&p);
-    println!("permutated = \n{:?}", &pa);
-    all_close(pa,
-              arr2(&[[1., 4., 7., 10.], [3., 6., 9., 12.], [2., 5., 8., 11.]]));
-}
-
-#[test]
-fn permutate_3x4_t() {
-    let a = arr2(&[[1., 5., 9.], [2., 6., 10.], [3., 7., 11.], [4., 8., 12.]]).reversed_axes();
-    println!("a= \n{:?}", &a);
-    let p = vec![1, 3, 3]; // replace 2-3
-    println!("permutation = \n{:?}", &p);
-    let pa = a.permutated(&p);
-    println!("permutated = \n{:?}", &pa);
-    all_close(pa,
-              arr2(&[[1., 2., 3., 4.], [9., 10., 11., 12.], [5., 6., 7., 8.]]));
-}
-
-#[test]
-fn permutate_4x3() {
-    let a = arr2(&[[1., 5., 9.], [2., 6., 10.], [3., 7., 11.], [4., 8., 12.]]);
-    println!("a= \n{:?}", &a);
-    let p = vec![4, 2, 3, 4]; // replace 1-4
-    println!("permutation = \n{:?}", &p);
-    let pa = a.permutated(&p);
-    println!("permutated = \n{:?}", &pa);
-    all_close(pa,
-              arr2(&[[4., 8., 12.], [2., 6., 10.], [3., 7., 11.], [1., 5., 9.]]))
-}
-
-#[test]
-fn permutate_4x3_t() {
-    let a = arr2(&[[1., 4., 7., 10.], [2., 5., 8., 11.], [3., 6., 9., 12.]]).reversed_axes();
-    println!("a= \n{:?}", &a);
-    let p = vec![4, 2, 3, 4]; // replace 1-4
-    println!("permutation = \n{:?}", &p);
-    let pa = a.permutated(&p);
-    println!("permutated = \n{:?}", &pa);
-    all_close(pa,
-              arr2(&[[10., 11., 12.], [4., 5., 6.], [7., 8., 9.], [1., 2., 3.]]))
-}
+test_permutate!(permutate,
+                vec![2, 2, 3],
+                &[[1., 2., 3.], [4., 5., 6.], [7., 8., 9.]],
+                &[[4., 5., 6.], [1., 2., 3.], [7., 8., 9.]]);
+test_permutate_t!(permutate_t,
+                  vec![2, 2, 3],
+                  &[[1., 4., 7.], [2., 5., 8.], [3., 6., 9.]],
+                  &[[4., 5., 6.], [1., 2., 3.], [7., 8., 9.]]);
+test_permutate!(permutate_3x4,
+                vec![1, 3, 3],
+                &[[1., 4., 7., 10.], [2., 5., 8., 11.], [3., 6., 9., 12.]],
+                &[[1., 4., 7., 10.], [3., 6., 9., 12.], [2., 5., 8., 11.]]);
+test_permutate_t!(permutate_3x4_t,
+                  vec![1, 3, 3],
+                  &[[1., 5., 9.], [2., 6., 10.], [3., 7., 11.], [4., 8., 12.]],
+                  &[[1., 2., 3., 4.], [9., 10., 11., 12.], [5., 6., 7., 8.]]);
+test_permutate!(permutate_4x3,
+                vec![4, 2, 3, 4],
+                &[[1., 5., 9.], [2., 6., 10.], [3., 7., 11.], [4., 8., 12.]],
+                &[[4., 8., 12.], [2., 6., 10.], [3., 7., 11.], [1., 5., 9.]]);
+test_permutate_t!(permutate_4x3_t,
+                  vec![4, 2, 3, 4],
+                  &[[1., 4., 7., 10.], [2., 5., 8., 11.], [3., 6., 9., 12.]],
+                  &[[10., 11., 12.], [4., 5., 6.], [7., 8., 9.], [1., 2., 3.]]);
 
 #[test]
 fn lu_square_upper() {

211e4c8e8b5136651fc89d919c51e21a8e6716bf
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-11-24 12:50:53 +0900

    Update doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,30 @@
-//! This crate serves matrix manipulation for [rust-ndarray](https://github.com/bluss/rust-ndarray)
+//! This crate serves matrix manipulation for
+//! [rust-ndarray](https://github.com/bluss/rust-ndarray).
+//!
 //! They are implemented as traits,
-//! [Matrix](matrix/trait.Matrix.html), [SquareMatrix](square/trait.SquareMatrix.html), and
+//! [Matrix](matrix/trait.Matrix.html), [SquareMatrix](square/trait.SquareMatrix.html),
+//! [TriangularMatrix](triangular/trait.TriangularMatrix.html), and
 //! [HermiteMatrix](hermite/trait.HermiteMatrix.html)
 //!
 //! Matrix
 //! -------
 //! - [singular-value decomposition](matrix/trait.Matrix.html#tymethod.svd)
+//! - [LU decomposition](matrix/trait.Matrix.html#tymethod.lu)
 //! - [QR decomposition](matrix/trait.Matrix.html#tymethod.qr)
 //! - [operator norm for L1 norm](matrix/trait.Matrix.html#tymethod.norm_1)
 //! - [operator norm for L-inf norm](matrix/trait.Matrix.html#tymethod.norm_i)
 //! - [Frobeiuns norm](matrix/trait.Matrix.html#tymethod.norm_f)
-//! - [LU factorization](matrix/trait.Matrix.html#tymethod.lu)
 //!
 //! SquareMatrix
 //! -------------
 //! - [inverse of matrix](square/trait.SquareMatrix.html#tymethod.inv)
 //! - [trace of matrix](square/trait.SquareMatrix.html#tymethod.trace)
 //! - [WIP] eigenvalue
+//!
+//! TriangularMatrix
+//! ------------------
+//! - [solve linear problem with upper triangular matrix](triangular/trait.TriangularMatrix.html#tymethod.solve_upper)
+//! - [solve linear problem with lower triangular matrix](triangular/trait.TriangularMatrix.html#tymethod.solve_lower)
 //!
 //! HermiteMatrix
 //! --------------

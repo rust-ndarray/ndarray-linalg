64ca50fbae0f507aafcacd5f2e3f74ea352bc5fe
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-01-29 23:19:19 +0900

    Split test for permutate

--- tests/lu.rs
+++ tests/lu.rs
@@ -1,56 +1,5 @@
 include!("header.rs");
 
-macro_rules! test_permutate {
-    ($testname:ident, $permutate:expr, $input:expr, $answer:expr) => {
-#[test]
-fn $testname() {
-    let a = arr2($input);
-    println!("a= \n{:?}", &a);
-    let p = $permutate; // replace 1-2
-    let pa = a.permutated(&p);
-    println!("permutated = \n{:?}", &pa);
-    all_close_l2(&pa, &arr2($answer), 1e-7).unwrap();
-}
-}} // end test_permutate
-
-macro_rules! test_permutate_t {
-    ($testname:ident, $permutate:expr, $input:expr, $answer:expr) => {
-#[test]
-fn $testname() {
-    let a = arr2($input).reversed_axes();
-    println!("a= \n{:?}", &a);
-    let p = $permutate; // replace 1-2
-    let pa = a.permutated(&p);
-    println!("permutated = \n{:?}", &pa);
-    all_close_l2(&pa, &arr2($answer), 1e-7).unwrap();
-}
-}} // end test_permutate_t
-
-test_permutate!(permutate,
-                vec![2, 2, 3],
-                &[[1., 2., 3.], [4., 5., 6.], [7., 8., 9.]],
-                &[[4., 5., 6.], [1., 2., 3.], [7., 8., 9.]]);
-test_permutate_t!(permutate_t,
-                  vec![2, 2, 3],
-                  &[[1., 4., 7.], [2., 5., 8.], [3., 6., 9.]],
-                  &[[4., 5., 6.], [1., 2., 3.], [7., 8., 9.]]);
-test_permutate!(permutate_3x4,
-                vec![1, 3, 3],
-                &[[1., 4., 7., 10.], [2., 5., 8., 11.], [3., 6., 9., 12.]],
-                &[[1., 4., 7., 10.], [3., 6., 9., 12.], [2., 5., 8., 11.]]);
-test_permutate_t!(permutate_3x4_t,
-                  vec![1, 3, 3],
-                  &[[1., 5., 9.], [2., 6., 10.], [3., 7., 11.], [4., 8., 12.]],
-                  &[[1., 2., 3., 4.], [9., 10., 11., 12.], [5., 6., 7., 8.]]);
-test_permutate!(permutate_4x3,
-                vec![4, 2, 3, 4],
-                &[[1., 5., 9.], [2., 6., 10.], [3., 7., 11.], [4., 8., 12.]],
-                &[[4., 8., 12.], [2., 6., 10.], [3., 7., 11.], [1., 5., 9.]]);
-test_permutate_t!(permutate_4x3_t,
-                  vec![4, 2, 3, 4],
-                  &[[1., 4., 7., 10.], [2., 5., 8., 11.], [3., 6., 9., 12.]],
-                  &[[10., 11., 12.], [4., 5., 6.], [7., 8., 9.], [1., 2., 3.]]);
-
 fn test_lu(a: Array<f64, Ix2>) {
     println!("a = \n{:?}", &a);
     let (p, l, u) = a.clone().lu().unwrap();

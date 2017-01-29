1f7cc56ab5021e2d0d3928ec6574797c18cd8124
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-01-29 16:57:18 +0900

    Test for opnorm

--- tests/opnorm.rs
+++ tests/opnorm.rs
@@ -1,55 +1,39 @@
 include!("header.rs");
 
+macro_rules! impl_test {
+    ($funcname:ident, $a:expr, $op1:expr, $opi:expr, $opf:expr) => {
 #[test]
-fn matrix_opnorm_square() {
-    let a = Array::range(1., 10., 1.).into_shape((3, 3)).unwrap();
-    a.opnorm_1().assert_close(18.0, 1e-7);
-    a.opnorm_i().assert_close(24.0, 1e-7);
-    a.opnorm_f().assert_close(285.0.sqrt(), 1e-7);
+fn $funcname() {
+    let a = $a;
+    a.opnorm_1().assert_close($op1, 1e-7);
+    a.opnorm_i().assert_close($opi, 1e-7);
+    a.opnorm_f().assert_close($opf, 1e-7);
 }
+}} // impl_test
 
-#[test]
-fn matrix_opnorm_square_t() {
-    let a = Array::range(1., 10., 1.).into_shape((3, 3)).unwrap().reversed_axes();
-    a.opnorm_1().assert_close(24.0, 1e-7);
-    a.opnorm_i().assert_close(18.0, 1e-7);
-    a.opnorm_f().assert_close(285.0.sqrt(), 1e-7);
+macro_rules! impl_test_opnorm {
+    ($modname:ident, $array:ty, $range:path) => {
+mod $modname {
+    use ndarray::prelude::*;
+    use ndarray_linalg::prelude::*;
+    use ndarray_numtest::prelude::*;
+    use num_traits::Float;
+    fn gen(i: usize, j: usize, rev: bool) -> $array {
+        let n = (i * j + 1) as f64;
+        if rev {
+            $range(1., n, 1.).into_shape((j, i)).unwrap().reversed_axes()
+        } else {
+            $range(1., n, 1.).into_shape((i, j)).unwrap()
+        }
+    }
+    impl_test!(opnorm_square, gen(3, 3, false), 18.0, 24.0, 285.0.sqrt());
+    impl_test!(opnorm_square_t, gen(3, 3, true), 24.0, 18.0, 285.0.sqrt());
+    impl_test!(opnorm_3x4, gen(3, 4, false), 24.0, 42.0, 650.0.sqrt());
+    impl_test!(opnorm_4x3_t, gen(4, 3, true), 42.0, 24.0, 650.0.sqrt());
+    impl_test!(opnorm_3x4_t, gen(3, 4, true), 33.0, 30.0, 650.0.sqrt());
+    impl_test!(opnorm_4x3, gen(4, 3, false), 30.0, 33.0, 650.0.sqrt());
 }
+}} // impl_test_opnorm
 
-#[test]
-fn matrix_opnorm_3x4() {
-    let a = Array::range(1., 13., 1.).into_shape((3, 4)).unwrap();
-    a.opnorm_1().assert_close(24.0, 1e-7);
-    a.opnorm_i().assert_close(42.0, 1e-7);
-    a.opnorm_f().assert_close(650.0.sqrt(), 1e-7);
-}
-
-#[test]
-fn matrix_opnorm_3x4_t() {
-    let a = Array::range(1., 13., 1.)
-        .into_shape((3, 4))
-        .unwrap()
-        .reversed_axes();
-    a.opnorm_1().assert_close(42.0, 1e-7);
-    a.opnorm_i().assert_close(24.0, 1e-7);
-    a.opnorm_f().assert_close(650.0.sqrt(), 1e-7);
-}
-
-#[test]
-fn matrix_opnorm_4x3() {
-    let a = Array::range(1., 13., 1.).into_shape((4, 3)).unwrap();
-    a.opnorm_1().assert_close(30.0, 1e-7);
-    a.opnorm_i().assert_close(33.0, 1e-7);
-    a.opnorm_f().assert_close(650.0.sqrt(), 1e-7);
-}
-
-#[test]
-fn matrix_opnorm_4x3_t() {
-    let a = Array::range(1., 13., 1.)
-        .into_shape((4, 3))
-        .unwrap()
-        .reversed_axes();
-    a.opnorm_1().assert_close(33.0, 1e-7);
-    a.opnorm_i().assert_close(30.0, 1e-7);
-    a.opnorm_f().assert_close(650.0.sqrt(), 1e-7);
-}
+impl_test_opnorm!(owned, Array<f64, Ix2>, Array::range);
+impl_test_opnorm!(shared, RcArray<f64, Ix2>, RcArray::range);

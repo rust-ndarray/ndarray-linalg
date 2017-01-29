7e5164ccd16e4fae6c8b9c1a043a666c4833fa82
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-01-29 16:21:35 +0900

    Test of deth for RcArray

--- tests/det.rs
+++ tests/det.rs
@@ -1,16 +1,21 @@
 include!("header.rs");
 
-fn random_hermite(n: usize) -> Array<f64, Ix2> {
-    let r_dist = RealNormal::new(0., 1.);
-    let a = Array::<f64, _>::random((n, n), r_dist);
-    a.dot(&a.t())
+macro_rules! impl_test_det {
+    ($modname:ident, $clone:ident) => {
+mod $modname {
+    use super::random_hermite;
+    use ndarray_linalg::prelude::*;
+    use ndarray_numtest::prelude::*;
+    #[test]
+    fn deth() {
+        let a = random_hermite(3);
+        let (e, _) = a.$clone().eigh().unwrap();
+        let deth = a.$clone().deth().unwrap();
+        let det_eig = e.iter().fold(1.0, |x, y| x * y);
+        deth.assert_close(det_eig, 1.0e-7);
+    }
 }
+}} // impl_test_det
 
-#[test]
-fn deth() {
-    let a = random_hermite(3);
-    let (e, _) = a.clone().eigh().unwrap();
-    let deth = a.clone().deth().unwrap();
-    let det_eig = e.iter().fold(1.0, |x, y| x * y);
-    deth.assert_close(det_eig, 1.0e-7);
-}
+impl_test_det!(owned, clone);
+impl_test_det!(shared, to_shared);
--- tests/header.rs
+++ tests/header.rs
@@ -17,3 +17,9 @@ use ndarray_numtest::prelude::*;
 use ndarray_rand::RandomExt;
 #[allow(unused_imports)]
 use num_traits::Float;
+
+pub fn random_hermite(n: usize) -> Array<f64, Ix2> {
+    let r_dist = RealNormal::new(0., 1.);
+    let a = Array::<f64, _>::random((n, n), r_dist);
+    a.dot(&a.t())
+}

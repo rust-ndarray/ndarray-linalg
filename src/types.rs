6923d72a7513f612932d49d8633b2d64a3d3002b
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-04-20 21:28:55 +0900

    Associated types

--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,31 @@
 
-use num_complex::Complex;
+pub use num_complex::Complex32 as c32;
+pub use num_complex::Complex64 as c64;
+use num_traits::Float;
+use std::ops::*;
 
-pub use num_comlex::Complex32 as c32;
-pub use num_comlex::Complex64 as c64;
+pub trait AssociatedReal: Sized {
+    type Real: Float + Mul<Self, Output = Self>;
+}
+pub trait AssociatedComplex: Sized {
+    type Complex;
+}
+
+macro_rules! impl_assoc {
+    ($real:ty, $complex:ty) => {
+impl AssociatedReal for $real {
+    type Real = $real;
+}
+impl AssociatedReal for $complex {
+    type Real = $real;
+}
+impl AssociatedComplex for $real {
+    type Complex = $complex;
+}
+impl AssociatedComplex for $complex {
+    type Complex = $complex;
+}
+}} // impl_assoc!
+
+impl_assoc!(f64, c64);
+impl_assoc!(f32, c32);

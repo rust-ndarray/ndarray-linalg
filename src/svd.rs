368d577c9111b1bd3eb2b71f7fabbcd9ece23a0e
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-06-05 16:39:53 +0900

    Add SVD impl for &ArrayBase, &mut ArrayBase

--- src/svd.rs
+++ src/svd.rs
@@ -20,6 +20,38 @@ impl<A, S, Su, Svt, Ss> SVD<ArrayBase<Su, Ix2>, ArrayBase<Ss, Ix1>, ArrayBase<Sv
            calc_u: bool,
            calc_vt: bool)
            -> Result<(Option<ArrayBase<Su, Ix2>>, ArrayBase<Ss, Ix1>, Option<ArrayBase<Svt, Ix2>>)> {
+        (&mut self).svd(calc_u, calc_vt)
+    }
+}
+
+impl<'a, A, S, Su, Svt, Ss> SVD<ArrayBase<Su, Ix2>, ArrayBase<Ss, Ix1>, ArrayBase<Svt, Ix2>> for &'a ArrayBase<S, Ix2>
+    where A: LapackScalar + Clone,
+          S: Data<Elem = A>,
+          Su: DataOwned<Elem = A>,
+          Svt: DataOwned<Elem = A>,
+          Ss: DataOwned<Elem = A::Real>
+{
+    fn svd(self,
+           calc_u: bool,
+           calc_vt: bool)
+           -> Result<(Option<ArrayBase<Su, Ix2>>, ArrayBase<Ss, Ix1>, Option<ArrayBase<Svt, Ix2>>)> {
+        let a = self.to_owned();
+        a.svd(calc_u, calc_vt)
+    }
+}
+
+impl<'a, A, S, Su, Svt, Ss> SVD<ArrayBase<Su, Ix2>, ArrayBase<Ss, Ix1>, ArrayBase<Svt, Ix2>>
+    for &'a mut ArrayBase<S, Ix2>
+    where A: LapackScalar,
+          S: DataMut<Elem = A>,
+          Su: DataOwned<Elem = A>,
+          Svt: DataOwned<Elem = A>,
+          Ss: DataOwned<Elem = A::Real>
+{
+    fn svd(mut self,
+           calc_u: bool,
+           calc_vt: bool)
+           -> Result<(Option<ArrayBase<Su, Ix2>>, ArrayBase<Ss, Ix1>, Option<ArrayBase<Svt, Ix2>>)> {
         let l = self.layout()?;
         let svd_res = A::svd(l, calc_u, calc_vt, self.as_allocated_mut()?)?;
         let (n, m) = l.size();

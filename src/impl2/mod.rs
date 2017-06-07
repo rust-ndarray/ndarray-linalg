06d6f76439ef576331a172022b91148c075df201
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-06-07 16:58:52 +0900

    Impl solve

--- src/impl2/mod.rs
+++ src/impl2/mod.rs
@@ -2,10 +2,22 @@
 pub mod opnorm;
 pub mod qr;
 pub mod svd;
+pub mod solve;
 
 pub use self::opnorm::*;
 pub use self::qr::*;
 pub use self::svd::*;
+pub use self::solve::*;
+
+use super::error::*;
 
 pub trait LapackScalar: OperatorNorm_ + QR_ + SVD_ {}
 impl<A> LapackScalar for A where A: OperatorNorm_ + QR_ + SVD_ {}
+
+pub fn into_result<T>(info: i32, val: T) -> Result<T> {
+    if info == 0 {
+        Ok(val)
+    } else {
+        Err(LapackError::new(info).into())
+    }
+}

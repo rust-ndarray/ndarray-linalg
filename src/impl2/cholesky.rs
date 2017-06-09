58f3775a0e3f6f788ede46428aaf1d9fa0f3722b
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-06-10 03:14:01 +0900

    Add Cholesky impl

---
+++ src/impl2/cholesky.rs
@@ -0,0 +1,29 @@
+//! implement Cholesky decomposition
+
+use lapack::c;
+
+use types::*;
+use error::*;
+use layout::Layout;
+
+use super::into_result;
+
+pub trait Cholesky_: Sized {
+    fn cholesky(Layout, a: &mut [Self]) -> Result<()>;
+}
+
+macro_rules! impl_cholesky {
+    ($scalar:ty, $potrf:path) => {
+impl Cholesky_ for $scalar {
+    fn cholesky(l: Layout, mut a: &mut [Self]) -> Result<()> {
+        let (n, _) = l.size();
+        let info = $potrf(l.lapacke_layout(), b'U', n, &mut a, n);
+        into_result(info, ())
+    }
+}
+}} // end macro_rules
+
+impl_cholesky!(f64, c::dpotrf);
+impl_cholesky!(f32, c::spotrf);
+impl_cholesky!(c64, c::zpotrf);
+impl_cholesky!(c32, c::cpotrf);
--- src/impl2/mod.rs
+++ src/impl2/mod.rs
@@ -3,16 +3,18 @@ pub mod opnorm;
 pub mod qr;
 pub mod svd;
 pub mod solve;
+pub mod cholesky;
 
 pub use self::opnorm::*;
 pub use self::qr::*;
 pub use self::svd::*;
 pub use self::solve::*;
+pub use self::cholesky::*;
 
 use super::error::*;
 
-pub trait LapackScalar: OperatorNorm_ + QR_ + SVD_ + Solve_ {}
-impl<A> LapackScalar for A where A: OperatorNorm_ + QR_ + SVD_ + Solve_ {}
+pub trait LapackScalar: OperatorNorm_ + QR_ + SVD_ + Solve_ + Cholesky_ {}
+impl<A> LapackScalar for A where A: OperatorNorm_ + QR_ + SVD_ + Solve_ + Cholesky_ {}
 
 pub fn into_result<T>(info: i32, val: T) -> Result<T> {
     if info == 0 {

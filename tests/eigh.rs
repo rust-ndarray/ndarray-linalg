ecd0643bdf52b7ab2014222d7fc1e4b0dfc08432
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-06-11 17:22:32 +0900

    Revise tests

--- tests/eigh.rs
+++ tests/eigh.rs
@@ -1,32 +1,29 @@
-include!("header.rs");
 
-macro_rules! impl_test {
-    ($modname:ident, $clone:ident) => {
-mod $modname {
-    use ndarray::prelude::*;
-    use ndarray_linalg::prelude::*;
-    #[test]
-    fn eigen_vector_manual() {
-        let a = arr2(&[[3.0, 1.0, 1.0], [1.0, 3.0, 1.0], [1.0, 1.0, 3.0]]);
-        let (e, vecs) = a.$clone().eigh().unwrap();
-        assert_close_l2!(&e, &arr1(&[2.0, 2.0, 5.0]), 1.0e-7);
-        for (i, v) in vecs.axis_iter(Axis(1)).enumerate() {
-            let av = a.dot(&v);
-            let ev = v.mapv(|x| e[i] * x);
-            assert_close_l2!(&av, &ev, 1.0e-7);
-        }
-    }
-    #[test]
-    fn diagonalize() {
-        let a = arr2(&[[3.0, 1.0, 1.0], [1.0, 3.0, 1.0], [1.0, 1.0, 3.0]]);
-        let (e, vecs) = a.$clone().eigh().unwrap();
-        let s = vecs.t().dot(&a).dot(&vecs);
-        for i in 0..3 {
-            assert_rclose!(e[i], s[(i, i)], 1e-7);
-        }
+extern crate ndarray;
+#[macro_use]
+extern crate ndarray_linalg;
+
+use ndarray::prelude::*;
+use ndarray_linalg::prelude::*;
+
+#[test]
+fn eigen_vector_manual() {
+    let a = arr2(&[[3.0, 1.0, 1.0], [1.0, 3.0, 1.0], [1.0, 1.0, 3.0]]);
+    let (e, vecs): (Array1<_>, Array2<_>) = (&a).eigh(UPLO::Upper).unwrap();
+    assert_close_l2!(&e, &arr1(&[2.0, 2.0, 5.0]), 1.0e-7);
+    for (i, v) in vecs.axis_iter(Axis(1)).enumerate() {
+        let av = a.dot(&v);
+        let ev = v.mapv(|x| e[i] * x);
+        assert_close_l2!(&av, &ev, 1.0e-7);
     }
 }
-}} // impl_test
 
-impl_test!(owned, clone);
-impl_test!(shared, to_shared);
+#[test]
+fn diagonalize() {
+    let a = arr2(&[[3.0, 1.0, 1.0], [1.0, 3.0, 1.0], [1.0, 1.0, 3.0]]);
+    let (e, vecs): (Array1<_>, Array2<_>) = (&a).eigh(UPLO::Upper).unwrap();
+    let s = vecs.t().dot(&a).dot(&vecs);
+    for i in 0..3 {
+        assert_rclose!(e[i], s[(i, i)], 1e-7);
+    }
+}

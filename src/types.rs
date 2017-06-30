e43fb41c71686dd4e26442397bed0cd4452a725c
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-06-30 11:50:44 +0900

    Add from_f64, into_real

--- src/types.rs
+++ src/types.rs
@@ -42,10 +42,31 @@ impl Scalar for f64 {}
 impl Scalar for c32 {}
 impl Scalar for c64 {}
 
-pub trait RealScalar: Scalar + Float + Sum {}
+pub trait RealScalar: Scalar + Float + Sum {
+    fn from_f64(f64) -> Self;
+}
 
-impl RealScalar for f32 {}
-impl RealScalar for f64 {}
+impl RealScalar for f32 {
+    fn from_f64(f: f64) -> Self {
+        f as f32
+    }
+}
+impl RealScalar for f64 {
+    fn from_f64(f: f64) -> Self {
+        f
+    }
+}
+
+/// Convert `f64` into `RealScalar`
+///
+/// ```rust
+/// fn mult2<A: RealScalar>(a: A) -> A {
+///     a * into_real(2.0);
+/// }
+/// ```
+pub fn into_real<T: RealScalar>(f: f64) -> T {
+    T::from_f64(f)
+}
 
 /// Define associating real float type
 pub trait AssociatedReal: Sized {

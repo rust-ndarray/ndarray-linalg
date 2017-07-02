018803e5c8d8cfc3ff174fbf4dc7aa99c95474df
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-07-01 18:05:42 -0600

    Unify implement of Eigh and EigVals

--- src/eigh.rs
+++ src/eigh.rs
@@ -3,7 +3,6 @@
 use ndarray::*;
 
 use super::UPLO;
-use super::convert::*;
 use super::diagonal::*;
 use super::error::*;
 use super::layout::*;
@@ -37,8 +36,8 @@ where
     type EigVal = Array1<A::Real>;
 
     fn eigh_into(mut self, uplo: UPLO) -> Result<(Self::EigVal, Self)> {
-        let s = A::eigh(true, self.square_layout()?, uplo, self.as_allocated_mut()?)?;
-        Ok((Array::from_vec(s), self))
+        let (val, _) = self.eigh_mut(uplo)?;
+        Ok((val, self))
     }
 }
 
@@ -51,9 +50,8 @@ where
     type EigVec = Array2<A>;
 
     fn eigh(&self, uplo: UPLO) -> Result<(Self::EigVal, Self::EigVec)> {
-        let mut a = replicate(self);
-        let s = A::eigh(true, a.square_layout()?, uplo, a.as_allocated_mut()?)?;
-        Ok((ArrayBase::from_vec(s), a))
+        let a = self.to_owned();
+        a.eigh_into(uplo)
     }
 }
 
@@ -71,42 +69,56 @@ where
 }
 
 /// Calculate eigenvalues without eigenvectors
-pub trait EigValsh<EigVal> {
-    fn eigvalsh(self, UPLO) -> Result<EigVal>;
+pub trait EigValsh {
+    type EigVal;
+    fn eigvalsh(&self, UPLO) -> Result<Self::EigVal>;
 }
 
-impl<A, S, Se> EigValsh<ArrayBase<Se, Ix1>> for ArrayBase<S, Ix2>
+/// Calculate eigenvalues without eigenvectors
+pub trait EigValshInto {
+    type EigVal;
+    fn eigvalsh_into(self, UPLO) -> Result<Self::EigVal>;
+}
+
+/// Calculate eigenvalues without eigenvectors
+pub trait EigValshMut {
+    type EigVal;
+    fn eigvalsh_mut(&mut self, UPLO) -> Result<Self::EigVal>;
+}
+
+impl<A, S> EigValshInto for ArrayBase<S, Ix2>
 where
     A: Scalar,
     S: DataMut<Elem = A>,
-    Se: DataOwned<Elem = A::Real>,
 {
-    fn eigvalsh(mut self, uplo: UPLO) -> Result<ArrayBase<Se, Ix1>> {
-        let s = A::eigh(false, self.square_layout()?, uplo, self.as_allocated_mut()?)?;
-        Ok(ArrayBase::from_vec(s))
+    type EigVal = Array1<A::Real>;
+
+    fn eigvalsh_into(mut self, uplo: UPLO) -> Result<Self::EigVal> {
+        self.eigvalsh_mut(uplo)
     }
 }
 
-impl<'a, A, S, Se> EigValsh<ArrayBase<Se, Ix1>> for &'a ArrayBase<S, Ix2>
+impl<A, S> EigValsh for ArrayBase<S, Ix2>
 where
     A: Scalar,
     S: Data<Elem = A>,
-    Se: DataOwned<Elem = A::Real>,
 {
-    fn eigvalsh(self, uplo: UPLO) -> Result<ArrayBase<Se, Ix1>> {
-        let mut a = self.to_owned();
-        let s = A::eigh(false, a.square_layout()?, uplo, a.as_allocated_mut()?)?;
-        Ok(ArrayBase::from_vec(s))
+    type EigVal = Array1<A::Real>;
+
+    fn eigvalsh(&self, uplo: UPLO) -> Result<Self::EigVal> {
+        let a = self.to_owned();
+        a.eigvalsh_into(uplo)
     }
 }
 
-impl<'a, A, S, Se> EigValsh<ArrayBase<Se, Ix1>> for &'a mut ArrayBase<S, Ix2>
+impl<A, S> EigValshMut for ArrayBase<S, Ix2>
 where
     A: Scalar,
     S: DataMut<Elem = A>,
-    Se: DataOwned<Elem = A::Real>,
 {
-    fn eigvalsh(mut self, uplo: UPLO) -> Result<ArrayBase<Se, Ix1>> {
+    type EigVal = Array1<A::Real>;
+
+    fn eigvalsh_mut(&mut self, uplo: UPLO) -> Result<Self::EigVal> {
         let s = A::eigh(true, self.square_layout()?, uplo, self.as_allocated_mut()?)?;
         Ok(ArrayBase::from_vec(s))
     }
@@ -126,10 +138,8 @@ where
     type Output = Array2<A>;
 
     fn ssqrt(&self, uplo: UPLO) -> Result<Self::Output> {
-        let (e, v) = self.eigh(uplo)?;
-        let e_sqrt = Array1::from_iter(e.iter().map(|r| AssociatedReal::inject(r.sqrt())));
-        let ev = e_sqrt.into_diagonal().op(&v.t());
-        Ok(v.op(&ev))
+        let a = self.to_owned();
+        a.ssqrt_into(uplo)
     }
 }
 

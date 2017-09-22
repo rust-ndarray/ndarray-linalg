1b17b65aad168f9afa5539cab725a5e2723179a1
Jim Turner <jturner314@gmail.com>
2017-09-21 20:56:17 -0400

    Remove redundant test
    The body of `cholesky_t()` was identical to the body of `cholesky()`.

--- tests/cholesky.rs
+++ tests/cholesky.rs
@@ -15,13 +15,3 @@ fn cholesky() {
     println!("cc = \n{:?}", c.t().dot(&c));
     assert_close_l2!(&c.t().dot(&c), &a, 1e-7);
 }
-
-#[test]
-fn cholesky_t() {
-    let a: Array2<f64> = random_hpd(3);
-    println!("a = \n{:?}", a);
-    let c: Array2<_> = (&a).cholesky(UPLO::Upper).unwrap();
-    println!("c = \n{:?}", c);
-    println!("cc = \n{:?}", c.t().dot(&c));
-    assert_close_l2!(&c.t().dot(&c), &a, 1e-7);
-}

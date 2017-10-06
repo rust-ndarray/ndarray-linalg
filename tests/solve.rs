cb7c2fc07b46f6c88e3b58da26094ae90f73e356
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-10-06 13:44:48 +0900

    Add test for solve Ax=b

--- tests/solve.rs
+++ tests/solve.rs
@@ -139,3 +139,21 @@ fn det_nonsquare() {
         }
     }
 }
+
+#[test]
+fn solve_random() {
+    let a: Array2<f64> = random((3, 3));
+    let x: Array1<f64> = random(3);
+    let b = a.dot(&x);
+    let y = a.solve_into(b).unwrap();
+    assert_close_l2!(&x, &y, 1e-7);
+}
+
+#[test]
+fn solve_random_t() {
+    let a: Array2<f64> = random((3, 3).f());
+    let x: Array1<f64> = random(3);
+    let b = a.dot(&x);
+    let y = a.solve_into(b).unwrap();
+    assert_close_l2!(&x, &y, 1e-7);
+}

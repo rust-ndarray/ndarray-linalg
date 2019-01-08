6e63d07396562d99edb0ab849cd23f5db0a29b77
Jim Turner <git@turner.link>
2019-01-08 17:07:51 -0500

    Switch from failure::Fail to std's Error trait

--- src/error.rs
+++ src/error.rs
@@ -1,35 +1,49 @@
 //! Define Errors
 
 use ndarray::{Ixs, ShapeError};
+use std::error;
+use std::fmt;
 
 pub type Result<T> = ::std::result::Result<T, LinalgError>;
 
 /// Master Error type of this crate
-#[derive(Fail, Debug)]
+#[derive(Debug)]
 pub enum LinalgError {
     /// Matrix is not square
-    #[fail(display = "Not square: rows({}) != cols({})", rows, cols)]
     NotSquare { rows: i32, cols: i32 },
-
     /// LAPACK subroutine returns non-zero code
-    #[fail(display = "LAPACK: return_code = {}", return_code)]
-    LapackFailure { return_code: i32 },
-
+    Lapack { return_code: i32 },
     /// Strides of the array is not supported
-    #[fail(display = "invalid stride: s0={}, s1={}", s0, s1)]
     InvalidStride { s0: Ixs, s1: Ixs },
-
     /// Memory is not aligned continously
-    #[fail(display = "Memory is not contiguous")]
-    MemoryNotCont {},
-
+    MemoryNotCont,
     /// Strides of the array is not supported
-    #[fail(display = "Shape Error: {}", error)]
-    ShapeFailure { error: ShapeError },
+    Shape(ShapeError),
+}
+
+impl fmt::Display for LinalgError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            LinalgError::NotSquare { rows, cols } => write!(f, "Not square: rows({}) != cols({})", rows, cols),
+            LinalgError::Lapack { return_code } => write!(f, "LAPACK: return_code = {}", return_code),
+            LinalgError::InvalidStride { s0, s1 } => write!(f, "invalid stride: s0={}, s1={}", s0, s1),
+            LinalgError::MemoryNotCont => write!(f, "Memory is not contiguous"),
+            LinalgError::Shape(err) => write!(f, "Shape Error: {}", err),
+        }
+    }
+}
+
+impl error::Error for LinalgError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match self {
+            LinalgError::Shape(err) => Some(err),
+            _ => None,
+        }
+    }
 }
 
 impl From<ShapeError> for LinalgError {
     fn from(error: ShapeError) -> LinalgError {
-        LinalgError::ShapeFailure { error }
+        LinalgError::Shape(error)
     }
 }

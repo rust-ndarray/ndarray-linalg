f9bf4a074416802968c6333fdccccf3b29746047
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-06-11 21:56:54 +0900

    Remove intel_mkl_src extern

--- src/lib.rs
+++ src/lib.rs
@@ -36,9 +36,6 @@
 //!  - [Random matrix generators](generate/index.html)
 //!  - [Scalar trait](types/trait.Scalar.html)
 
-#[cfg(feature = "intel-mkl")]
-extern crate intel_mkl_src;
-
 pub mod assert;
 pub mod cholesky;
 pub mod convert;

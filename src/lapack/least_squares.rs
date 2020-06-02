49263f5a3080ce660bb689e30efc915b6a92be3d
Paul Koerbitz <koerbitz@google.com>
2020-06-03 00:04:12 +0200

    First step implementing least squares for multiple RHS

--- src/lapack/least_squares.rs
+++ src/lapack/least_squares.rs
@@ -25,6 +25,14 @@ pub trait LeastSquaresSvdDivideConquer_: Scalar {
         a: &mut [Self],
         b: &mut [Self],
     ) -> Result<LeastSquaresOutput<Self>>;
+
+    unsafe fn least_squares_nrhs(
+        a_layout: MatrixLayout,
+        a: &mut [Self],
+        b_layout: MatrixLayout,
+        b: &mut [Self],
+    ) -> Result<LeastSquaresOutput<Self>>;
+
 }
 
 macro_rules! impl_least_squares {
@@ -58,6 +66,55 @@ macro_rules! impl_least_squares {
                     // this is the 'leading dimension of b', in the case where
                     // b is a single vector, this should be 1
                     nrhs,
+                    &mut singular_values,
+                    rcond,
+                    &mut rank,
+                );
+
+                // FIXME: make use of this fact (from lapack docs)
+                // https://www.netlib.org/lapack/explore-html/d7/d3b/group__double_g_esolve_ga94bd4a63a6dacf523e25ff617719f752
+                // could check how this is done by scipy here: https://github.com/scipy/scipy/blob/v1.4.1/scipy/linalg/basic.py#L1047-L1246
+                // If m >= n and RANK = n, the residual
+                // sum-of-squares for the solution in the i-th column is given
+                // by the sum of squares of elements n+1:m in that column.
+                into_result(
+                    status,
+                    LeastSquaresOutput {
+                        singular_values,
+                        rank,
+                    },
+                )
+            }
+            
+            unsafe fn least_squares_nrhs(
+                a_layout: MatrixLayout,
+                a: &mut [Self],
+                b_layout: MatrixLayout,
+                b: &mut [Self],
+            ) -> Result<LeastSquaresOutput<Self>> {
+                let (m, n) = a_layout.size();
+                if m != b_layout.size().0 || a_layout.lapacke_layout() != b_layout.lapacke_layout() {
+                    return Err(LinalgError::Shape(ShapeError::from_kind(
+                        ErrorKind::IncompatibleShape,
+                    )));
+                }
+                let k = ::std::cmp::min(m, n);
+                let nrhs = 1;
+                let rcond: Self::Real = -1.;
+                let mut singular_values: Vec<Self::Real> = vec![Self::Real::zero(); k as usize];
+                let mut rank: i32 = 0;
+
+                let status = $gelsd(
+                    a_layout.lapacke_layout(),
+                    m,
+                    n,
+                    nrhs,
+                    a,
+                    a_layout.lda(),
+                    b,
+                    // this is the 'leading dimension of b', in the case where
+                    // b is a single vector, this should be 1
+                    b_layout.lda(),
                     &mut singular_values,
                     rcond,
                     &mut rank,
--- src/least_squares.rs
+++ src/least_squares.rs
@@ -13,65 +13,65 @@ use crate::layout::*;
 use crate::types::*;
 
 /// Result of LeastSquares
-pub struct LeastSquaresResult<A: Scalar> {
+pub struct LeastSquaresResult<A: Scalar, I> {
     /// singular values
     pub singular_values: Array1<A::Real>,
     /// The solution matrix
-    pub solution: Array1<A>,
+    pub solution: Array<A, I>,
     /// The rank of the input matrix A
     pub rank: i32,
 }
 
-pub trait LeastSquaresSvd<S, A>
+pub trait LeastSquaresSvd<S, A, I>
 where
     S: Data<Elem = A>,
     A: Scalar + Lapack,
 {
-    fn least_squares(&self, rhs: &ArrayBase<S, Ix1>) -> Result<LeastSquaresResult<A>>;
+    fn least_squares(&self, rhs: &ArrayBase<S, I>) -> Result<LeastSquaresResult<A, I>>;
 }
 
-pub trait LeastSquaresSvdInto<S, A>
+pub trait LeastSquaresSvdInto<S, A, I>
 where
     S: Data<Elem = A>,
     A: Scalar + Lapack,
 {
-    fn least_squares_into(self, rhs: ArrayBase<S, Ix1>) -> Result<LeastSquaresResult<A>>;
+    fn least_squares_into(self, rhs: ArrayBase<S, I>) -> Result<LeastSquaresResult<A, I>>;
 }
 
-pub trait LeastSquaresSvdInPlace<S, A>
+pub trait LeastSquaresSvdInPlace<S, A, I>
 where
     S: Data<Elem = A>,
     A: Scalar + Lapack,
 {
     fn least_squares_in_place(
         &mut self,
-        rhs: &mut ArrayBase<S, Ix1>,
-    ) -> Result<LeastSquaresResult<A>>;
+        rhs: &mut ArrayBase<S, I>,
+    ) -> Result<LeastSquaresResult<A, I>>;
 }
 
-impl<A, S> LeastSquaresSvd<S, A> for ArrayBase<S, Ix2>
+impl<A, S> LeastSquaresSvd<S, A, Ix1> for ArrayBase<S, Ix2>
 where
     A: Scalar + Lapack + LeastSquaresSvdDivideConquer_,
     S: Data<Elem = A>,
 {
-    fn least_squares(&self, rhs: &ArrayBase<S, Ix1>) -> Result<LeastSquaresResult<A>> {
+    fn least_squares(&self, rhs: &ArrayBase<S, Ix1>) -> Result<LeastSquaresResult<A, Ix1>> {
         let a = self.to_owned();
         let b = rhs.to_owned();
         a.least_squares_into(b)
     }
 }
 
-impl<A, S> LeastSquaresSvdInto<S, A> for ArrayBase<S, Ix2>
+impl<A, S> LeastSquaresSvdInto<S, A, Ix1> for ArrayBase<S, Ix2>
 where
     A: Scalar + Lapack + LeastSquaresSvdDivideConquer_,
     S: DataMut<Elem = A>,
 {
-    fn least_squares_into(mut self, mut rhs: ArrayBase<S, Ix1>) -> Result<LeastSquaresResult<A>> {
+    fn least_squares_into(mut self, mut rhs: ArrayBase<S, Ix1>) -> Result<LeastSquaresResult<A, Ix1>> {
         self.least_squares_in_place(&mut rhs)
     }
 }
 
-impl<A, S> LeastSquaresSvdInPlace<S, A> for ArrayBase<S, Ix2>
+impl<A, S> LeastSquaresSvdInPlace<S, A, Ix1> for ArrayBase<S, Ix2>
 where
     A: Scalar + Lapack + LeastSquaresSvdDivideConquer_,
     S: DataMut<Elem = A>,
@@ -79,7 +79,7 @@ where
     fn least_squares_in_place(
         &mut self,
         rhs: &mut ArrayBase<S, Ix1>,
-    ) -> Result<LeastSquaresResult<A>> {
+    ) -> Result<LeastSquaresResult<A, Ix1>> {
         let a_layout = self.layout()?;
         let LeastSquaresOutput::<A> {
             singular_values,
@@ -102,7 +102,39 @@ where
         })
     }
 }
- 
+
+impl<A, S> LeastSquaresSvdInPlace<S, A, Ix2> for ArrayBase<S, Ix2>
+where
+    A: Scalar + Lapack + LeastSquaresSvdDivideConquer_,
+    S: DataMut<Elem = A>,
+{
+    fn least_squares_in_place(
+        &mut self,
+        rhs: &mut ArrayBase<S, Ix2>,
+    ) -> Result<LeastSquaresResult<A, Ix2>> {
+        let a_layout = self.layout()?;
+        let rhs_layout = rhs.layout()?;
+        let LeastSquaresOutput::<A> {
+            singular_values,
+            rank,
+        } = unsafe {
+            <A as LeastSquaresSvdDivideConquer_>::least_squares_nrhs(
+                a_layout,
+                self.as_allocated_mut()?,
+                rhs_layout,
+                rhs.as_allocated_mut()?
+            )?
+        };
+
+        let solution = rhs.to_owned();
+        Ok(LeastSquaresResult {
+            solution,
+            singular_values: Array::from_shape_vec((singular_values.len(),), singular_values)?,
+            rank,
+        })
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -128,6 +160,18 @@ mod tests {
         let b: Array1<f64> = array![-3., 14., 12., 16., 16.];
         let expected: Array1<f64> = array![1.,1.,2.];
         let result = a.least_squares(&b).unwrap();
+        
+        println!(" *****\nresult: {}\nexpected: {}\n *****", result.solution, expected);
+        assert!(result.solution.abs_diff_eq(&expected, 1e-12));
+    }
+
+    #[test]
+    fn netlib_lapack_example_for_dgels_nrhs() {
+        // https://www.netlib.org/lapack/lapacke.html#_calling_code_dgels_code
+        let mut a: Array2<f64> = array![[1.,1.,1.],[2.,3.,4.], [3.,5.,2. ],[4.,2.,5.],[5.,4.,3.]];
+        let mut b: Array2<f64> = array![[-10.,12.,14.,16.,18.], [-3., 14., 12., 16., 16.]].t().to_owned();
+        let expected: Array2<f64> = array![[2., 1.],[1., 1.], [1.,2.]];
+        let result = a.least_squares_in_place(&mut b).unwrap();
         
         println!(" *****\nresult: {}\nexpected: {}\n *****", result.solution, expected);
         assert!(result.solution.abs_diff_eq(&expected, 1e-12));

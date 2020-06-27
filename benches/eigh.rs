2680794d20962e85fcc51dd7c4d01caf88884b06
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-06-27 14:46:00 +0900

    Use bench_with_input

--- benches/eigh.rs
+++ benches/eigh.rs
@@ -4,16 +4,15 @@ use ndarray_linalg::*;
 
 fn eigh_small(c: &mut Criterion) {
     let mut group = c.benchmark_group("eigh");
-
     for &n in &[4, 8, 16, 32, 64, 128] {
-        group.bench_function(&format!("eigh{:03}", n), |b| {
-            let a: Array2<f64> = random((n, n));
+        group.bench_with_input(BenchmarkId::new("eigh", n), &n, |b, n| {
+            let a: Array2<f64> = random((*n, *n));
             b.iter(|| {
                 let (_e, _vecs) = a.eigh(UPLO::Upper).unwrap();
             })
         });
-        group.bench_function(&format!("eigh{:03}_t", n), |b| {
-            let a: Array2<f64> = random((n, n).f());
+        group.bench_with_input(BenchmarkId::new("eigh_t", n), &n, |b, n| {
+            let a: Array2<f64> = random((*n, *n).f());
             b.iter(|| {
                 let (_e, _vecs) = a.eigh(UPLO::Upper).unwrap();
             })
--- benches/truncated_eig.rs
+++ benches/truncated_eig.rs
@@ -5,14 +5,14 @@ use ndarray_linalg::*;
 fn truncated_eigh_small(c: &mut Criterion) {
     let mut group = c.benchmark_group("truncated_eigh");
     for &n in &[4, 8, 16, 32, 64, 128] {
-        group.bench_function(&format!("truncated_eig{:03}", n), |b| {
+        group.bench_with_input(BenchmarkId::new("truncated_eig", n), &n, |b, &n| {
             let a: Array2<f64> = random((n, n));
             let a = a.t().dot(&a);
             b.iter(move || {
                 let _result = TruncatedEig::new(a.clone(), TruncatedOrder::Largest).decompose(1);
             })
         });
-        group.bench_function(&format!("truncated_eig{:03}_t", n), |b| {
+        group.bench_with_input(BenchmarkId::new("truncated_eig_t", n), &n, |b, &n| {
             let a: Array2<f64> = random((n, n).f());
             let a = a.t().dot(&a);
             b.iter(|| {

1aeb4c1ecea5349c9df7bbc6c5ad0c5dd5e3ad7d
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-06-27 14:16:59 +0900

    Enable grouping for truncated_eigh

--- benches/eigh.rs
+++ benches/eigh.rs
@@ -6,13 +6,13 @@ fn eigh_small(c: &mut Criterion) {
     let mut group = c.benchmark_group("eigh");
 
     for &n in &[4, 8, 16, 32, 64, 128] {
-        group.bench_function(&format!("eigh{}", n), |b| {
+        group.bench_function(&format!("eigh{:03}", n), |b| {
             let a: Array2<f64> = random((n, n));
             b.iter(|| {
                 let (_e, _vecs) = a.eigh(UPLO::Upper).unwrap();
             })
         });
-        group.bench_function(&format!("eigh{}_t", n), |b| {
+        group.bench_function(&format!("eigh{:03}_t", n), |b| {
             let a: Array2<f64> = random((n, n).f());
             b.iter(|| {
                 let (_e, _vecs) = a.eigh(UPLO::Upper).unwrap();
--- benches/truncated_eig.rs
+++ benches/truncated_eig.rs
@@ -1,41 +1,26 @@
-#[macro_use]
-extern crate criterion;
-
-use criterion::Criterion;
+use criterion::*;
 use ndarray::*;
 use ndarray_linalg::*;
 
-macro_rules! impl_teig {
-    ($n:expr) => {
-        paste::item! {
-            fn [<teig $n>](c: &mut Criterion) {
-                c.bench_function(&format!("truncated_eig{}", $n), |b| {
-                    let a: Array2<f64> = random(($n, $n));
-                    let a = a.t().dot(&a);
-
-                    b.iter(move || {
-                        let _result = TruncatedEig::new(a.clone(), TruncatedOrder::Largest).decompose(1);
-                    })
-                });
-                c.bench_function(&format!("truncated_eig{}_t", $n), |b| {
-                    let a: Array2<f64> = random(($n, $n).f());
-                    let a = a.t().dot(&a);
-
-                    b.iter(|| {
-                        let _result = TruncatedEig::new(a.clone(), TruncatedOrder::Largest).decompose(1);
-                    })
-                });
-            }
-        }
-    };
+fn truncated_eigh_small(c: &mut Criterion) {
+    let mut group = c.benchmark_group("truncated_eigh");
+    for &n in &[4, 8, 16, 32, 64, 128] {
+        group.bench_function(&format!("truncated_eig{:03}", n), |b| {
+            let a: Array2<f64> = random((n, n));
+            let a = a.t().dot(&a);
+            b.iter(move || {
+                let _result = TruncatedEig::new(a.clone(), TruncatedOrder::Largest).decompose(1);
+            })
+        });
+        group.bench_function(&format!("truncated_eig{:03}_t", n), |b| {
+            let a: Array2<f64> = random((n, n).f());
+            let a = a.t().dot(&a);
+            b.iter(|| {
+                let _result = TruncatedEig::new(a.clone(), TruncatedOrder::Largest).decompose(1);
+            })
+        });
+    }
 }
 
-impl_teig!(4);
-impl_teig!(8);
-impl_teig!(16);
-impl_teig!(32);
-impl_teig!(64);
-impl_teig!(128);
-
-criterion_group!(teig, teig4, teig8, teig16, teig32, teig64, teig128);
-criterion_main!(teig);
+criterion_group!(truncated_eigh, truncated_eigh_small);
+criterion_main!(truncated_eigh);

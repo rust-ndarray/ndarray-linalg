001a46aa79201565cdd2420b4e69e15533204088
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-06-27 13:59:25 +0900

    Enable grouping for benchmarking

--- benches/eigh.rs
+++ benches/eigh.rs
@@ -1,37 +1,25 @@
-#[macro_use]
-extern crate criterion;
-
-use criterion::Criterion;
+use criterion::*;
 use ndarray::*;
 use ndarray_linalg::*;
 
-macro_rules! impl_eigh {
-    ($n:expr) => {
-        paste::item! {
-            fn [<eigh $n>](c: &mut Criterion) {
-                c.bench_function(&format!("eigh{}", $n), |b| {
-                    let a: Array2<f64> = random(($n, $n));
-                    b.iter(|| {
-                        let (_e, _vecs) = a.eigh(UPLO::Upper).unwrap();
-                    })
-                });
-                c.bench_function(&format!("eigh{}_t", $n), |b| {
-                    let a: Array2<f64> = random(($n, $n).f());
-                    b.iter(|| {
-                        let (_e, _vecs) = a.eigh(UPLO::Upper).unwrap();
-                    })
-                });
-            }
-        }
-    };
+fn eigh_small(c: &mut Criterion) {
+    let mut group = c.benchmark_group("eigh");
+
+    for &n in &[4, 8, 16, 32, 64, 128] {
+        group.bench_function(&format!("eigh{}", n), |b| {
+            let a: Array2<f64> = random((n, n));
+            b.iter(|| {
+                let (_e, _vecs) = a.eigh(UPLO::Upper).unwrap();
+            })
+        });
+        group.bench_function(&format!("eigh{}_t", n), |b| {
+            let a: Array2<f64> = random((n, n).f());
+            b.iter(|| {
+                let (_e, _vecs) = a.eigh(UPLO::Upper).unwrap();
+            })
+        });
+    }
 }
 
-impl_eigh!(4);
-impl_eigh!(8);
-impl_eigh!(16);
-impl_eigh!(32);
-impl_eigh!(64);
-impl_eigh!(128);
-
-criterion_group!(eigh, eigh4, eigh8, eigh16, eigh32, eigh64, eigh128);
+criterion_group!(eigh, eigh_small);
 criterion_main!(eigh);

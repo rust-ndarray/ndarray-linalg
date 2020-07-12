cb3010784aceccd36ee46735d7d8f56c379a309a
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-07-12 17:16:51 +0900

    Split real/complex of svddc

--- lax/src/svddc.rs
+++ lax/src/svddc.rs
@@ -21,7 +21,7 @@ pub trait SVDDC_: Scalar {
     unsafe fn svddc(l: MatrixLayout, jobz: UVTFlag, a: &mut [Self]) -> Result<SVDOutput<Self>>;
 }
 
-macro_rules! impl_svdd {
+macro_rules! impl_svddc_real {
     ($scalar:ty, $gesdd:path) => {
         impl SVDDC_ for $scalar {
             unsafe fn svddc(
@@ -70,7 +70,57 @@ macro_rules! impl_svdd {
     };
 }
 
-impl_svdd!(f32, lapacke::sgesdd);
-impl_svdd!(f64, lapacke::dgesdd);
-impl_svdd!(c32, lapacke::cgesdd);
-impl_svdd!(c64, lapacke::zgesdd);
+impl_svddc_real!(f32, lapacke::sgesdd);
+impl_svddc_real!(f64, lapacke::dgesdd);
+
+macro_rules! impl_svddc_complex {
+    ($scalar:ty, $gesdd:path) => {
+        impl SVDDC_ for $scalar {
+            unsafe fn svddc(
+                l: MatrixLayout,
+                jobz: UVTFlag,
+                mut a: &mut [Self],
+            ) -> Result<SVDOutput<Self>> {
+                let (m, n) = l.size();
+                let k = m.min(n);
+                let lda = l.lda();
+                let (ucol, vtrow) = match jobz {
+                    UVTFlag::Full => (m, n),
+                    UVTFlag::Some => (k, k),
+                    UVTFlag::None => (1, 1),
+                };
+                let mut s = vec![Self::Real::zero(); k.max(1) as usize];
+                let mut u = vec![Self::zero(); (m * ucol).max(1) as usize];
+                let ldu = l.resized(m, ucol).lda();
+                let mut vt = vec![Self::zero(); (vtrow * n).max(1) as usize];
+                let ldvt = l.resized(vtrow, n).lda();
+                $gesdd(
+                    l.lapacke_layout(),
+                    jobz as u8,
+                    m,
+                    n,
+                    &mut a,
+                    lda,
+                    &mut s,
+                    &mut u,
+                    ldu,
+                    &mut vt,
+                    ldvt,
+                )
+                .as_lapack_result()?;
+                Ok(SVDOutput {
+                    s,
+                    u: if jobz == UVTFlag::None { None } else { Some(u) },
+                    vt: if jobz == UVTFlag::None {
+                        None
+                    } else {
+                        Some(vt)
+                    },
+                })
+            }
+        }
+    };
+}
+
+impl_svddc_complex!(c32, lapacke::cgesdd);
+impl_svddc_complex!(c64, lapacke::zgesdd);

e3a77678d789e6938a641433134dfd8a50acf2be
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-07-15 21:46:24 +0900

    Merge impl_eigh! and impl_eighc!

--- lax/src/eigh.rs
+++ lax/src/eigh.rs
@@ -25,7 +25,13 @@ pub trait Eigh_: Scalar {
 }
 
 macro_rules! impl_eigh {
-    ($scalar:ty, $ev:path, $evg:path) => {
+    (@real, $scalar:ty, $ev:path, $evg:path) => {
+        impl_eigh!(@body, $scalar, $ev, $evg, );
+    };
+    (@complex, $scalar:ty, $ev:path, $evg:path) => {
+        impl_eigh!(@body, $scalar, $ev, $evg, rwork);
+    };
+    (@body, $scalar:ty, $ev:path, $evg:path, $($rwork_ident:ident),*) => {
         impl Eigh_ for $scalar {
             fn eigh(
                 calc_v: bool,
@@ -37,11 +43,14 @@ macro_rules! impl_eigh {
                 let n = layout.len();
                 let jobz = if calc_v { b'V' } else { b'N' };
                 let mut eigs = vec![Self::Real::zero(); n as usize];
-                let n = n as i32;
+
+                $(
+                let mut $rwork_ident = vec![Self::Real::zero(); 3 * n as usize - 2];
+                )*
 
                 // calc work size
                 let mut info = 0;
-                let mut work_size = [0.0];
+                let mut work_size = [Self::zero()];
                 unsafe {
                     $ev(
                         jobz,
@@ -52,6 +61,7 @@ macro_rules! impl_eigh {
                         &mut eigs,
                         &mut work_size,
                         -1,
+                        $(&mut $rwork_ident,)*
                         &mut info,
                     );
                 }
@@ -70,6 +80,7 @@ macro_rules! impl_eigh {
                         &mut eigs,
                         &mut work,
                         lwork as i32,
+                        $(&mut $rwork_ident,)*
                         &mut info,
                     );
                 }
@@ -88,11 +99,14 @@ macro_rules! impl_eigh {
                 let n = layout.len();
                 let jobz = if calc_v { b'V' } else { b'N' };
                 let mut eigs = vec![Self::Real::zero(); n as usize];
-                let n = n as i32;
+
+                $(
+                let mut $rwork_ident = vec![Self::Real::zero(); 3 * n as usize - 2];
+                )*
 
                 // calc work size
                 let mut info = 0;
-                let mut work_size = [0.0];
+                let mut work_size = [Self::zero()];
                 unsafe {
                     $evg(
                         &[1],
@@ -106,6 +120,7 @@ macro_rules! impl_eigh {
                         &mut eigs,
                         &mut work_size,
                         -1,
+                        $(&mut $rwork_ident,)*
                         &mut info,
                     );
                 }
@@ -127,6 +142,7 @@ macro_rules! impl_eigh {
                         &mut eigs,
                         &mut work,
                         lwork as i32,
+                        $(&mut $rwork_ident,)*
                         &mut info,
                     );
                 }
@@ -137,85 +153,7 @@ macro_rules! impl_eigh {
     };
 } // impl_eigh!
 
-impl_eigh!(f64, lapack::dsyev, lapack::dsygv);
-impl_eigh!(f32, lapack::ssyev, lapack::ssygv);
-
-// splitted for RWORK
-macro_rules! impl_eighc {
-    ($scalar:ty, $ev:path, $evg:path) => {
-        impl Eigh_ for $scalar {
-            fn eigh(
-                calc_v: bool,
-                layout: MatrixLayout,
-                uplo: UPLO,
-                mut a: &mut [Self],
-            ) -> Result<Vec<Self::Real>> {
-                assert_eq!(layout.len(), layout.lda());
-                let n = layout.len();
-                let jobz = if calc_v { b'V' } else { b'N' };
-                let mut eigs = vec![Self::Real::zero(); n as usize];
-                let mut work = vec![Self::zero(); 2 * n as usize - 1];
-                let mut rwork = vec![Self::Real::zero(); 3 * n as usize - 2];
-                let mut info = 0;
-                let n = n as i32;
-
-                unsafe {
-                    $ev(
-                        jobz,
-                        uplo as u8,
-                        n,
-                        &mut a,
-                        n,
-                        &mut eigs,
-                        &mut work,
-                        2 * n - 1,
-                        &mut rwork,
-                        &mut info,
-                    )
-                };
-                info.as_lapack_result()?;
-                Ok(eigs)
-            }
-
-            fn eigh_generalized(
-                calc_v: bool,
-                layout: MatrixLayout,
-                uplo: UPLO,
-                mut a: &mut [Self],
-                mut b: &mut [Self],
-            ) -> Result<Vec<Self::Real>> {
-                assert_eq!(layout.len(), layout.lda());
-                let n = layout.len();
-                let jobz = if calc_v { b'V' } else { b'N' };
-                let mut eigs = vec![Self::Real::zero(); n as usize];
-                let mut work = vec![Self::zero(); 2 * n as usize - 1];
-                let mut rwork = vec![Self::Real::zero(); 3 * n as usize - 2];
-                let n = n as i32;
-                let mut info = 0;
-
-                unsafe {
-                    $evg(
-                        &[1],
-                        jobz,
-                        uplo as u8,
-                        n,
-                        &mut a,
-                        n,
-                        &mut b,
-                        n,
-                        &mut eigs,
-                        &mut work,
-                        2 * n - 1,
-                        &mut rwork,
-                        &mut info,
-                    )
-                };
-                info.as_lapack_result()?;
-                Ok(eigs)
-            }
-        }
-    };
-} // impl_eigh!
-
-impl_eighc!(c64, lapack::zheev, lapack::zhegv);
-impl_eighc!(c32, lapack::cheev, lapack::chegv);
+impl_eigh!(@real, f64, lapack::dsyev, lapack::dsygv);
+impl_eigh!(@real, f32, lapack::ssyev, lapack::ssygv);
+impl_eigh!(@complex, c64, lapack::zheev, lapack::zhegv);
+impl_eigh!(@complex, c32, lapack::cheev, lapack::chegv);

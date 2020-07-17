e35234744e411b1087950e546a5486f0c09f1e20
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-07-17 20:48:22 +0900

    Drop unsafe of cholesky

--- lax/src/cholesky.rs
+++ lax/src/cholesky.rs
@@ -8,32 +8,37 @@ pub trait Cholesky_: Sized {
     /// Cholesky: wrapper of `*potrf`
     ///
     /// **Warning: Only the portion of `a` corresponding to `UPLO` is written.**
-    unsafe fn cholesky(l: MatrixLayout, uplo: UPLO, a: &mut [Self]) -> Result<()>;
+    fn cholesky(l: MatrixLayout, uplo: UPLO, a: &mut [Self]) -> Result<()>;
+
     /// Wrapper of `*potri`
     ///
     /// **Warning: Only the portion of `a` corresponding to `UPLO` is written.**
-    unsafe fn inv_cholesky(l: MatrixLayout, uplo: UPLO, a: &mut [Self]) -> Result<()>;
+    fn inv_cholesky(l: MatrixLayout, uplo: UPLO, a: &mut [Self]) -> Result<()>;
+
     /// Wrapper of `*potrs`
-    unsafe fn solve_cholesky(l: MatrixLayout, uplo: UPLO, a: &[Self], b: &mut [Self])
-        -> Result<()>;
+    fn solve_cholesky(l: MatrixLayout, uplo: UPLO, a: &[Self], b: &mut [Self]) -> Result<()>;
 }
 
 macro_rules! impl_cholesky {
     ($scalar:ty, $trf:path, $tri:path, $trs:path) => {
         impl Cholesky_ for $scalar {
-            unsafe fn cholesky(l: MatrixLayout, uplo: UPLO, a: &mut [Self]) -> Result<()> {
+            fn cholesky(l: MatrixLayout, uplo: UPLO, a: &mut [Self]) -> Result<()> {
                 let (n, _) = l.size();
-                $trf(l.lapacke_layout(), uplo as u8, n, a, n).as_lapack_result()?;
+                unsafe {
+                    $trf(l.lapacke_layout(), uplo as u8, n, a, n).as_lapack_result()?;
+                }
                 Ok(())
             }
 
-            unsafe fn inv_cholesky(l: MatrixLayout, uplo: UPLO, a: &mut [Self]) -> Result<()> {
+            fn inv_cholesky(l: MatrixLayout, uplo: UPLO, a: &mut [Self]) -> Result<()> {
                 let (n, _) = l.size();
-                $tri(l.lapacke_layout(), uplo as u8, n, a, l.lda()).as_lapack_result()?;
+                unsafe {
+                    $tri(l.lapacke_layout(), uplo as u8, n, a, l.lda()).as_lapack_result()?;
+                }
                 Ok(())
             }
 
-            unsafe fn solve_cholesky(
+            fn solve_cholesky(
                 l: MatrixLayout,
                 uplo: UPLO,
                 a: &[Self],
@@ -42,8 +47,10 @@ macro_rules! impl_cholesky {
                 let (n, _) = l.size();
                 let nrhs = 1;
                 let ldb = 1;
-                $trs(l.lapacke_layout(), uplo as u8, n, nrhs, a, l.lda(), b, ldb)
-                    .as_lapack_result()?;
+                unsafe {
+                    $trs(l.lapacke_layout(), uplo as u8, n, nrhs, a, l.lda(), b, ldb)
+                        .as_lapack_result()?;
+                }
                 Ok(())
             }
         }

2b60edfe8fdfa9ddbebbc4b2c21dc98071d18c0f
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-08-02 02:34:21 +0900

    Add svddc

--- ndarray-linalg/benches/svd.rs
+++ ndarray-linalg/benches/svd.rs
@@ -56,5 +56,47 @@ fn svd_small(c: &mut Criterion) {
     }
 }
 
-criterion_group!(svd, svd_small);
+fn svddc_small(c: &mut Criterion) {
+    let mut group = c.benchmark_group("svddc");
+    for &n in &[4, 8, 16, 32, 64, 128] {
+        group.bench_with_input(BenchmarkId::new("C", n), &n, |b, n| {
+            let a: Array2<f64> = random((*n, *n));
+            b.iter(|| {
+                let _ = a.svddc(UVTFlag::None).unwrap();
+            })
+        });
+        group.bench_with_input(BenchmarkId::new("F", n), &n, |b, n| {
+            let a: Array2<f64> = random((*n, *n).f());
+            b.iter(|| {
+                let _ = a.svddc(UVTFlag::None).unwrap();
+            })
+        });
+        group.bench_with_input(BenchmarkId::new("some/C", n), &n, |b, n| {
+            let a: Array2<f64> = random((*n, *n));
+            b.iter(|| {
+                let _ = a.svddc(UVTFlag::Some).unwrap();
+            })
+        });
+        group.bench_with_input(BenchmarkId::new("some/F", n), &n, |b, n| {
+            let a: Array2<f64> = random((*n, *n).f());
+            b.iter(|| {
+                let _ = a.svddc(UVTFlag::Some).unwrap();
+            })
+        });
+        group.bench_with_input(BenchmarkId::new("full/C", n), &n, |b, n| {
+            let a: Array2<f64> = random((*n, *n));
+            b.iter(|| {
+                let _ = a.svddc(UVTFlag::Full).unwrap();
+            })
+        });
+        group.bench_with_input(BenchmarkId::new("full/F", n), &n, |b, n| {
+            let a: Array2<f64> = random((*n, *n).f());
+            b.iter(|| {
+                let _ = a.svddc(UVTFlag::Full).unwrap();
+            })
+        });
+    }
+}
+
+criterion_group!(svd, svd_small, svddc_small);
 criterion_main!(svd);

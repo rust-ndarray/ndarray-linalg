76e757e5f568dd1a7e7863a8ee5177731edceac0
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-08-02 20:33:45 +0900

    QR, solveh

--- ndarray-linalg/benches/solve.rs
+++ ndarray-linalg/benches/solve.rs
@@ -2,23 +2,35 @@ use criterion::*;
 use ndarray::*;
 use ndarray_linalg::*;
 
-fn factorize_small(c: &mut Criterion) {
-    let mut group = c.benchmark_group("factorize");
+fn solve_small(c: &mut Criterion) {
+    let mut group = c.benchmark_group("solve");
     for &n in &[4, 8, 16, 32, 64, 128] {
-        group.bench_with_input(BenchmarkId::new("C", n), &n, |b, n| {
+        group.bench_with_input(BenchmarkId::new("factorize/C", n), &n, |b, n| {
             let a: Array2<f64> = random((*n, *n));
             b.iter(|| {
                 let _lu = a.factorize().unwrap();
             })
         });
-        group.bench_with_input(BenchmarkId::new("F", n), &n, |b, n| {
+        group.bench_with_input(BenchmarkId::new("factorize/F", n), &n, |b, n| {
             let a: Array2<f64> = random((*n, *n).f());
             b.iter(|| {
                 let _lu = a.factorize().unwrap();
             })
         });
+        group.bench_with_input(BenchmarkId::new("inv/C", n), &n, |b, n| {
+            let a: Array2<f64> = random((*n, *n));
+            b.iter(|| {
+                let _inv = a.inv().unwrap();
+            })
+        });
+        group.bench_with_input(BenchmarkId::new("inv/F", n), &n, |b, n| {
+            let a: Array2<f64> = random((*n, *n).f());
+            b.iter(|| {
+                let _inv = a.inv().unwrap();
+            })
+        });
     }
 }
 
-criterion_group!(factorize, factorize_small);
-criterion_main!(factorize);
+criterion_group!(solve, solve_small);
+criterion_main!(solve);

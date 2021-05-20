2364dac9412910a2c9b952b8d3a667b6c0aac2de
Jim Turner <git@turner.link>
2021-05-19 23:08:59 -0400

    Add tests for matching shape checks in SolveH

--- ndarray-linalg/tests/solveh.rs
+++ ndarray-linalg/tests/solveh.rs
@@ -1,6 +1,23 @@
 use ndarray::*;
 use ndarray_linalg::*;
 
+#[should_panic]
+#[test]
+fn solveh_shape_mismatch() {
+    let a: Array2<f64> = random_hpd(3);
+    let b: Array1<f64> = random(2);
+    let _ = a.solveh_into(b);
+}
+
+#[should_panic]
+#[test]
+fn factorizeh_solveh_shape_mismatch() {
+    let a: Array2<f64> = random_hpd(3);
+    let b: Array1<f64> = random(2);
+    let f = a.factorizeh_into().unwrap();
+    let _ = f.solveh_into(b);
+}
+
 #[test]
 fn solveh_random() {
     let a: Array2<f64> = random_hpd(3);
@@ -13,6 +30,23 @@ fn solveh_random() {
     let f = a.factorizeh_into().unwrap();
     let y = f.solveh_into(b).unwrap();
     assert_close_l2!(&x, &y, 1e-7);
+}
+
+#[should_panic]
+#[test]
+fn solveh_t_shape_mismatch() {
+    let a: Array2<f64> = random_hpd(3).reversed_axes();
+    let b: Array1<f64> = random(2);
+    let _ = a.solveh_into(b);
+}
+
+#[should_panic]
+#[test]
+fn factorizeh_solveh_t_shape_mismatch() {
+    let a: Array2<f64> = random_hpd(3).reversed_axes();
+    let b: Array1<f64> = random(2);
+    let f = a.factorizeh_into().unwrap();
+    let _ = f.solveh_into(b);
 }
 
 #[test]

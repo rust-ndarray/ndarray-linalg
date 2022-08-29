f0c88d93707cb3dcb8f72b5a4e71d8cc67d88121
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-08-29 14:48:33 +0900

    Re-parse lapack-sys crate

--- lax-bindgen/src/main.rs
+++ lax-bindgen/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use std::{fs, path::PathBuf};
+
+fn main() -> Result<()> {
+    let crate_root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let lapack_sys = fs::read_to_string(crate_root.join("lapack-sys/src/lapack.rs"))?;
+
+    let file: syn::File = syn::parse_str(&lapack_sys)?;
+    dbg!(file);
+
+    Ok(())
 }
